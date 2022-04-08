5852129eda6d81b4b7103c2ca79b51286f8daf41
Jakub Chilczuk <56444728+Alkhome@users.noreply.github.com>
2022-04-08 22:25:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+class client
+{
+    static void Main(string[] args)
+    {
+        String input;
+
+        using (TcpClient tcpClient =
+                new TcpClient("localhost", 1857))
+        using (NetworkStream networkStream =
+                tcpClient.GetStream())
+        using (StreamReader streamReader =
+                new StreamReader(networkStream))
+        {
+            input = streamReader.ReadToEnd();
+        }
+
+        Console.WriteLine("Received data: " + input + "\n");
+    }
+}
